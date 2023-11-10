58a64c0b66e1f454229bfe7e1ae0710c364f6e93
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-11-10 12:26:46 -0300

    Bugfix

--- assets/preload/stages/idiot.lua
+++ assets/preload/stages/idiot.lua
@@ -1,9 +1,7 @@
 function onCreate()
-	-- background shit
-	makeLuaSprite('white', 'white', -1400, -800);
-	setScrollFactor('white', 1.8, 1.8);
-
-	addLuaSprite('white', false);
-	
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+-- background shit bugfix by soulbf10
+makeLuaSprite('white', 'white', -580, -190)
+scaleObject('white', 1, 1)
+setScrollFactor('white', 1.8, 1.8)
+close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
